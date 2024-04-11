a2332a55b6aadd4e84f145fb0036398e0edfe9a3
Pirakalathan01 <pirakalathanpilla99@gmail.com>
2024-04-11 23:43:34 +0530

    url change

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,4 +3,4 @@ import env from "../config/env"
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.defaults.baseURL = env.url;
+axios.defaults.baseURL ="https://63gfn7h3naskfg4ivxuvpwqxde0mnmnc.lambda-url.ap-southeast-1.on.aws";
